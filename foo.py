d58b5c1f850ba285cc7c8df6420bff15b173e1e4
AdamTumbleR <Peter Toth>
2019-12-22 14:01:08 +0100

    Initial program
    First version

---
+++ foo.py
@@ -0,0 +1,22 @@
+import os
+
+path = r'/home/pi'
+
+extension=input('Extension: ')
+
+
+def filterByExtension(fileName, extension):
+    dottindex=0
+    for idx, i in enumerate(fileName):
+        if i=='.':
+            dottindex=idx
+    endExtension=fileName[dottindex+1:]
+    return endExtension == extension
+
+def listdir(dir):
+    fileNames = os.listdir(dir)
+    filteredFileNames = filter(lambda x: filterByExtension(x, extension),fileNames)
+    for fileNames in filteredFileNames:
+        print(fileNames)
+
+listdir(path)
